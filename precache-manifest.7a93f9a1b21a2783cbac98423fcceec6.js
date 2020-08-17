a44ccfb94f46fbc20b72803ade7171dcfd9e79f4
kaetaen <kaetaen@tutanota.com>
2020-08-17 03:00:54 -0300

    Updates

---
+++ precache-manifest.7a93f9a1b21a2783cbac98423fcceec6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51bfbb0642ab2b147c5556d4f19e8626",
+    "url": "/index.html"
+  },
+  {
+    "revision": "913bea74664af7d1c3ce",
+    "url": "/static/css/main.3600562d.chunk.css"
+  },
+  {
+    "revision": "4b846320770493619b25",
+    "url": "/static/js/2.1b51011b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1b51011b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913bea74664af7d1c3ce",
+    "url": "/static/js/main.701a79ce.chunk.js"
+  },
+  {
+    "revision": "3286a808bdfdd93558e1",
+    "url": "/static/js/runtime-main.432ccfdf.js"
+  },
+  {
+    "revision": "85171f09db835fcfb9fc51614b9d294d",
+    "url": "/static/media/back.85171f09.png"
+  }
+]);
